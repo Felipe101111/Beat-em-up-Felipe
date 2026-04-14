321311cb3c19e09ae72cc7a5b30ccbdf3156522f
Felipe101111 <felipevical@gmail.com>
2026-04-14 17:07:48 -0300

    enemigo persigue al prota

--- Beat'em up/Escenas/esqueleto.gd
+++ Beat'em up/Escenas/esqueleto.gd
@@ -1,41 +1,29 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var prota: CharacterBody2D = $"../prota"
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var dirrecion = (prota.global_position - global_position).normalized()
+	var distancia = global_position.distance_to(prota.global_position)
+	if distancia >75:
+		if prota.global_position.x < global_position.x :
+			velocity=dirrecion*SPEED
+			animated_sprite_2d.play("walk")
+			animated_sprite_2d.flip_h = true
+		else :
+			velocity=dirrecion*SPEED
+			animated_sprite_2d.play("walk")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if not is_on_floor():
-		animated_sprite_2d.play("jump")
-	elif velocity.x == 0 and Input.is_action_pressed("punch"):
+		velocity = Vector2(0,0)
 		animated_sprite_2d.play("ataque")
-	elif  direction > 0:
-		animated_sprite_2d.flip_h = false
-		animated_sprite_2d.play("walk")
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true
-		animated_sprite_2d.play("walk")
-	else:
-		animated_sprite_2d.play("idle")
-	
-
-	
+		
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	pass # Replace with function body.
